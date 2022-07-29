79dc666667fc6233bce2d246f7f0adcef01b36ac
Stongwill <mishabasha1997@gmail.com>
2022-07-29 09:03:56 +0300

    upgrade webpack

--- webpack.config.js
+++ webpack.config.js
@@ -14,6 +14,7 @@ module.exports = {
     filename: "[name].[contenthash].js", 
     path: path.resolve(__dirname, "dist"), 
     clean: true,
+    assetModuleFilename: 'assets/[name][ext]'
   },
   devtool: "source-map", 
   optimization: {
@@ -26,7 +27,7 @@ module.exports = {
   },
   plugins: [
     new HtmlWebpackPlugin({ template: "./index.html" }),
-    new MiniCssExtractPlugin({ filename: "style.[contenthash].css" }),
+    new MiniCssExtractPlugin({ filename: "[name].[contenthash].css" }),
   ],
   module: {
     rules: [
@@ -39,14 +40,52 @@ module.exports = {
         use: [
           mode === "development" ? "style-loader" : MiniCssExtractPlugin.loader,
           "css-loader",
+          {
+            loader: 'postcss-loader',
+            options: {
+              postcssOptions: {
+                plugins: [require('postcss-preset-env')],
+              }
+            }
+          },
           "sass-loader",
         ],
       },
       {
-        test: /\.(png|gif|svg|jpg|jpeg)$/i,
+        test: /\.(png|gif|svg|jpe?g|webp)$/i,
+        use: [
+          {
+            loader: 'image-webpack-loader',
+            options: {
+              mozjpeg: {
+                progressive: true,
+              },
+              // optipng.enabled: false will disable optipng
+              optipng: {
+                enabled: false,
+              },
+              pngquant: {
+                quality: [0.65, 0.90],
+                speed: 4
+              },
+              gifsicle: {
+                interlaced: false,
+              },
+              // the webp option will enable WEBP
+              webp: {
+                quality: 75
+              }
+            }
+          }
+        ],
+        type:'asset/resource',
       },
       {
         test: /\.(ttf|woff|woff2|eot|otf)$/i,
+        type:'asset/resource',
+        generator: {
+          filename: 'fonts/[name][ext]'
+        }
       },
     ],
   },
